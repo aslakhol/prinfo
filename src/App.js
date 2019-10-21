bbd4a404fd4481b596d816786ae7c32adf9648a0
Aslak Hollund <aslakhol@gmail.com>
2019-10-21 20:34:49 +0200

    include weather in app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import Falaffel from './Falaffel';
-import Clock from './Clock';
-import Bikes from './Bikes';
-import Busses from './Busses'
+import React from "react";
+import Falaffel from "./Falaffel";
+import Clock from "./Clock";
+import Bikes from "./Bikes";
+import Busses from "./Busses";
+import Weather from "./Weather";
 // import ApiTime from './ApiTime';
 
 const App = () => {
@@ -13,8 +14,9 @@ const App = () => {
       <Falaffel />
       <Bikes />
       <Busses />
+      <Weather />
     </div>
-  )
-}
+  );
+};
 
 export default App;
