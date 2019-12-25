7b6c14c56ff35dbadac023c26564e1d8a95d29bf
Aslak Hollund <aslakhol@gmail.com>
2019-12-25 17:27:42 +0100

    fix broken locale in clock

--- src/Clock.js
+++ src/Clock.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import { DateTime } from 'luxon';
+import React from "react";
+import { DateTime } from "luxon";
 
 const Clock = () => {
-  const dateTime = DateTime.local().setLocale("no-bok");
+  const dateTime = DateTime.local().setLocale("nb-NO");
 
   return (
     <>
-      <span>
-        {dateTime.toLocaleString(DateTime.TIME_24_SIMPLE)}
-      </span>
+      <span>{dateTime.toLocaleString(DateTime.TIME_24_SIMPLE)}</span>
       <br />
       <span>
-        {dateTime.toLocaleString(
-          {
-            weekday: 'long',
-            day: 'numeric',
-            month: 'long'
-          }
-        )}
+        {dateTime.toLocaleString({
+          weekday: "long",
+          day: "numeric",
+          month: "long"
+        })}
       </span>
     </>
-  )
-}
+  );
+};
 
 export default Clock;
