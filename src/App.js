cff1bc9b4f76c5d76535c8c5a5a0e8932dd1ce62
Aslak Hollund <aslakhol@gmail.com>
2020-06-11 21:34:26 +0200

    state in parent based api refreshment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import OpeningHours from "./OpeningHours";
 import Clock from "./Clock";
 import Bikes from "./Bikes";
@@ -9,6 +9,23 @@ import DadJokes from "./DadJokes";
 import "./App.css";
 
 const App = () => {
+  const [updated, setUpdated] = useState(1);
+  const APIRefreshDelay = 30000;
+
+  const refreshAPIData = () => {
+    console.log("APIs refreshed");
+
+    setUpdated((prevUpdated) => prevUpdated * -1);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("triggered");
+      refreshAPIData();
+    }, APIRefreshDelay);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="wrapper">
       {/* <ApiTime /> */}
