8015822f5310d392db1b2962b85f5cea8b6450d8
Aslak Hollund <aslakhol@gmail.com>
2021-11-01 07:41:24 +0100

    temp hide broken bikes thing

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
       <div className="wrapper">
         <Clock />
         <ControlledBusses />
-        <ControlledBikes />
+        {/* <ControlledBikes /> */}
         <ControlledWeather />
         <ControlledDadJokes />
       </div>
